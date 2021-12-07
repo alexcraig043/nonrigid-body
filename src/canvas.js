59b6e838320ff6dce7065b7924a8188d6319ad83
cregtheleg <alexcraig043@gmail.com>
2021-12-07 11:18:50 -0500

    Currently broken

---
+++ src/canvas.js
@@ -0,0 +1,164 @@
+let nodes = []
+let sticks = []
+let nodeRadius = 17
+let simulating = false
+let drawingStick = false
+let firstNode, secondNode
+let k = .001 // Spring Constant
+let gravityStrength = .001
+
+function setup()
+{
+    createCanvas(windowWidth, windowHeight)
+    angleMode(DEGREES)
+}
+
+function draw()
+{
+    clear()
+    background(51)
+    
+    drawElements();
+
+    if (simulating)
+    {
+        simulatePhysics();
+    }
+}
+
+// Handles physics simulations of gravity onto Nodes and spring force of Sticks onto Nodes
+function simulatePhysics()
+{
+    for (let node of nodes)
+    {
+        node.gravityForce(gravityStrength)
+    }
+
+    for (let stick of sticks)
+    {
+        stick.springForce()
+    }
+}
+
+// Draws all Elements
+function drawElements()
+{
+    if(drawingStick)
+    {
+        drawTempStick(firstNode)
+    }
+
+    for (let stick of sticks)
+    {
+        stick.show()
+    }
+
+    for (let node of nodes)
+    {
+        node.show()
+    }
+}
+
+// When mouse is pressed, runs logic for click event:
+function mousePressed()
+{
+    clickLogic();
+}
+
+// Handles logic for click event. Either draws new Node and starts drawing temporary Stick, or connects Stick being drawn to an existing Node
+function clickLogic()
+{
+    let clickedOnNode = false;
+    for (let node of nodes)
+    {
+        let d = dist(mouseX, mouseY, node.position.x, node.position.y)
+        {
+            // If user clicks on/near an existing node:
+            if (d < node.radius * 2)
+            {
+                if (!drawingStick)
+                {
+                    // If user is not currently drawing a Stick, then start drawing one from clicked on node
+                    firstNode = node
+                    drawingStick = true;
+                } 
+                else 
+                {
+                    // If user is currently drawing a temporary Stick, then finish the Stick at the clicked node and starting drawing a new temporary Stick from clicked Node as long as an identical stick doesn't already exist
+                    secondNode = node
+                    if (!stickAlreadyExists(firstNode, secondNode))
+                    {
+                        newStick(firstNode, secondNode)
+                        firstNode = secondNode
+                    }
+                }
+                clickedOnNode = true;
+            }
+        }
+    }
+    // If user didn't click on existing node and is not drawing a temporary Stick, then make a new Node
+    if (!clickedOnNode && !drawingStick)
+    {
+        newNode(mouseX, mouseY)
+    }
+}
+
+// If an identical Stick doesn't already exist, the generates a new Stick and appends it to the Sticks array:
+function newStick(firstNode, secondNode)
+{
+    sticks.push(new Stick(firstNode, secondNode, k))
+}
+
+// Checks for identical Stick:
+function stickAlreadyExists(a, b)
+{
+    for (let stick of sticks)
+    {
+        if (stick.nodeA == a && stick.nodeB == b || stick.nodeA == b && stick.nodeB == a)
+        {
+            return true
+        }
+    }
+}
+
+// Generates a new Node and appends it to the Nodes array, also handles logic of whether new Node is fixed:
+function newNode(x, y)
+{
+    let pos = createVector(x, y)
+    if (keyIsDown(SHIFT))
+    {
+        nodes.push(new Node(pos, true, nodeRadius))
+    } else
+    {
+        nodes.push(new Node(pos, false, nodeRadius))
+    }
+}
+
+// Draws a temporary Stick from previously clicked Node to current mouse position:
+function drawTempStick(fromNode)
+{
+    stroke('#fffafa')
+    strokeWeight(2)
+    line(fromNode.position.x, fromNode.position.y, mouseX, mouseY)
+}
+
+// Handles functionality of certain key-binds:
+function keyPressed()
+{
+    // Stops drawing a temporary Stick when escape is pressed:
+    if (keyCode == 27)
+    {
+        drawingStick = false;
+    }
+    // Toggles physics simulation when Space is pressed:
+    if (keyCode == 32)
+    {
+        simulating = !simulating;
+    }
+}
+
+// Re-runs setup() when window is resized:
+function windowResized()
+{
+    setup();
+}
---
+++ src/rope.js
@@ -0,0 +1,141 @@
+class Node
+{
+    constructor(pos, lock, rad)
+    {
+        this.acceleration = createVector(0,0)
+        this.velocity = createVector(0,0)
+        this.position = createVector(pos.x, pos.y)
+        this.locked = lock
+        this.radius = rad
+    }
+
+    show()
+    {
+        strokeWeight(0)
+        if (!this.locked)
+        {
+            fill('#fffafa') 
+        } 
+        else
+        {
+            fill('#e65a5a')        
+        }
+        circle(this.position.x, this.position.y, this.radius * 2)
+    }
+
+    // Handles the gravity in physics sim - Sticks can be assumed as massless so no gravity force on them
+    gravityForce(g)
+    {
+        let gravityVector = createVector(0,g);
+        this.updateNode(gravityVector)
+    }
+
+    // Adjusts acceleration, velocity, and position of Node when force is applied:
+    updateNode(force)
+    {
+        if(!this.locked)
+        {
+            this.acceleration.add(force.x, force.y)
+            this.velocity.add(this.acceleration)
+            this.position.add(this.velocity)    
+        }
+    }
+
+    setAcceleration(acc)
+    {
+        this.acceleration = acc
+    }
+}
+
+class Stick
+{
+    constructor(firstNode, secondNode, springConstant)
+    {
+        this.nodeA = firstNode
+        this.nodeB = secondNode
+        this.k = springConstant
+        this.equilibrium = dist(this.nodeA.position.x, this.nodeA.position.y, this.nodeB.position.x, this.nodeB.position.y)
+    }
+
+    show()
+    {
+        stroke('#fffafa')
+        strokeWeight(2)
+        line(this.nodeA.position.x, this.nodeA.position.y, this.nodeB.position.x, this.nodeB.position.y)
+    }
+
+    // Finds spring force of Stick using Hooke's law, then applies spring force onto Nodes
+    springForce()
+    {
+        // Use displacement and spring constant to calculate magnitude of spring force with Hooke's law
+        let springForceMagnitude = -this.k * this.getDisplacement()
+
+        // The magnitude of the x component of the spring force will be proportional to cos(theta), y component will be proportional to sin(theta)
+        let theta = this.getTheta()
+        let springForceMagnitudeX = abs(springForceMagnitude * cos(theta))
+        let springForceMagnitudeY = abs(springForceMagnitude * sin(theta))
+
+        // If springForceMagnitude is positive, then Stick will pull Nodes together, if springForceMagnitude is negative, then Stick will push Nodes apart
+        if (springForceMagnitude > 0)
+        {
+            if (this.nodeA.position.x > this.nodeB.position.x)
+            {
+                springForceMagnitudeX *= -1
+            }
+            if (this.nodeA.position.y > this.nodeB.position.y)
+            {
+                springForceMagnitudeY *= -1
+            }
+        }
+        if (springForceMagnitude < 0)
+        {
+            if (this.nodeA.position.x < this.nodeB.position.x)
+            {
+                springForceMagnitudeX *= -1
+            }
+            if (this.nodeA.position.y < this.nodeB.position.y)
+            {
+                springForceMagnitudeY *= -1
+            }
+        }
+
+        // Sets force vector to x and y components of spring force
+        let springForceVector = createVector(springForceMagnitudeX, springForceMagnitudeY)
+
+        // console.log("acceleration: ", this.nodeA.acceleration, "\nspring force: ", springForceVector, '\nsum: ', this.nodeA.acceleration.add(springForceVector))
+        console.log(springForceMagnitudeX)
+
+        // Adjust acceleration, velocity, and position of Nodes
+        this.nodeA.updateNode(springForceVector)
+        this.nodeB.updateNode(springForceVector.mult(-1))
+    }
+
+    // Returns the angle between Node A and Node B
+    getTheta()
+    {
+        let theta = atan(this.getDistanceY() / this.getDistanceX())
+        return theta    
+    }
+
+    // Returns the current displacement of Nodes compared to equilibrium
+    getDisplacement()
+    {
+        let distBetweenNodes = dist(this.nodeA.position.x, this.nodeA.position.y, this.nodeB.position.x, this.nodeB.position.y)
+        let displacement = this.equilibrium - distBetweenNodes
+        return displacement
+    }
+
+    // Returns current distance between Nodes in x plane
+    getDistanceX()
+    {
+        let distBetweenNodesX = abs(this.nodeA.position.x - this.nodeB.position.x)
+        return distBetweenNodesX
+    }
+
+    // Returns current distance between Nodes in y plane
+    getDistanceY()
+    {
+        let distBetweenNodesY = abs(this.nodeA.position.y - this.nodeB.position.y)
+        return distBetweenNodesY
+    }
+}
